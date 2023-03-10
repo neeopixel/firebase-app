dda2544422b0b65d6fb896af228247df9492771a
Sarup Das <neeopixel@gmail.com>
2023-03-10 18:06:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
       <BrowserRouter basename="/firebase-app">
         <Routes>
           <Route exact path="/" element={<Landing />}></Route>
-          <Route path="/Dashboard" element={<Dashboard />}></Route>
-          <Route path="/Signin" element={<Signin />}></Route>
-          <Route path="/Signup" element={<Signup />}></Route>
+          <Route exact path ="/Dashboard" element={<Dashboard />}></Route>
+          <Route exact path ="/Signin" element={<Signin />}></Route>
+          <Route exact path ="/Signup" element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
