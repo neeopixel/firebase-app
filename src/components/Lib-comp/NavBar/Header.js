b06ffe04f74c3e4d86b4a9e9eeffdce8d25043f5
Sarup Das <64528173+neeopixel@users.noreply.github.com>
2023-03-13 14:16:20 +0530

    Update Header.js

--- src/components/Lib-comp/NavBar/Header.js
+++ src/components/Lib-comp/NavBar/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import "./Header.module.css";
+import styles from"./Header.module.css";
 import Container from "react-bootstrap/Container";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -32,13 +32,13 @@ export default function Header() {
               src={Logo}
               width="30"
               height="30"
-              className="d-inline-block align-top"
+              className="styles.d-inline-block styles.align-top"
             />{" "}
             React Bootstrap
           </Link>
           </Navbar.Brand>
         </Container>
-        <Container className="justify-content-end">
+        <Container className="styles.justify-content-end">
           <Nav className="me-auto">
             <Nav.Link ><Link to="/Dashboard">Dashboard</Link></Nav.Link>
             <Nav.Link ><Link to="/Signin">Sign In</Link></Nav.Link>
